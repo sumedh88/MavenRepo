ee84ee8f4f0d5f7ed1fc297b833c08f21967812e
sumedh88 <sumedh_surlekar@persistent.com>
2016-07-06 10:43:27 +0530

    added files in repo

---
+++ src/main/java/org/psl/devops/App.java
@@ -0,0 +1,13 @@
+package org.psl.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
